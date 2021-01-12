c63007311e2820986813db7c725469dc04958898
Cornelius Enriquez <cornelius.enriquez.ce@gmail.com>
2021-01-12 18:07:59 +0800

    with rbt test cases

--- MP2-StarterCode/src/Main.java
+++ MP2-StarterCode/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println("Searching for 31: "+bst.search(31)); //31
         System.out.println("The minimum key is: "+bst.minimum()); //10
         System.out.println("The maximum key is: "+bst.maximum()); //42
-        System.out.println("The successor of 10 is: "+bst.successor(10)); //21
+        System.out.println("The successor of 10 is: "+ bst.successor(10)); //21
         System.out.println("The predecessor of 10 is: "+bst.predecessor(10)); //null
         System.out.println("Successor of 22 is " + bst.successor(22)); //31
         System.out.println("Predecessor of 22 is " + bst.predecessor(22)); //21
@@ -28,6 +28,40 @@ public class Main {
 
         System.out.println("Searching for 31: " + bst.search(31)); //null
         System.out.println("Searching for 10: " + bst.search(10)); //null
+        
+        RedBlackTree<Integer> rb = new RedBlackTree<Integer>();
+        rb.insert(7);
+        rb.insert(5);
+        rb.insert(3);
+        rb.insert(2);
+        rb.insert(4);
+        rb.insert(9);
+        rb.insert(1);
+        rb.delete(3);
+        System.out.println("Preorder: " + rb.preorder());
+        //5(black) 2(red) 1(black) 4(black) 7(black) 9(red)
+        System.out.println("Inorder: " + rb.inorder());
+        //1(black) 2(red) 4(black) 5(black) 7(black) 9(red) 
+        System.out.println("Postorder: " + rb.postorder());
+        //1(black) 4(black) 2(red) 9(red) 7(black) 5(black) 
+
+        RedBlackTree<String> rb2 = new RedBlackTree<String>();
+        rb2.insert("Sirius");
+        rb2.insert("Canopus");
+        rb2.insert("Alpha Centauri");
+        rb2.insert("Acherner");
+        rb2.insert("Arcturus");
+        rb2.insert("Betelgeuse");
+        rb2.insert("Capella");
+        rb2.insert("Rigel");
+        rb2.insert("Procyon");
+        rb2.insert("Vega");
+        rb2.delete("Canopus");
+
+        System.out.println("Preorder: " + rb2.preorder());
+        System.out.println("Inorder: " + rb2.inorder());
+        System.out.println("Postorder: " + rb2.postorder());
+
 
     }
 }
